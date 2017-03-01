effde75f8aca0aa7dad39430dc2f10cc64685a71
Zhixiang Li <Zhixiang Li@10.80.148.181>
2017-03-01 11:00:56 -0500

    Initialize project.

---
+++ src/edu/wmich/cs1120/pratice/Main.java
@@ -0,0 +1,11 @@
+package edu.wmich.cs1120.pratice;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("2");
+		main(args);
+	}
+
+}
